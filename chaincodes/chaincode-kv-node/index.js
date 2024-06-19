576c51cef8ba1d34f8894aee2878c9fcf148cadd
iqballm09 <22_iqbal@apps.ipb.ac.id>
2024-06-19 14:46:12 +0000

    update

---
+++ chaincodes/chaincode-kv-node/index.js
@@ -0,0 +1,146 @@
+const { Contract } = require("fabric-contract-api");
+const crypto = require("crypto");
+
+class DOContract extends Contract {
+    constructor() {
+        super("DOContract");
+    }
+
+    async instantiate() {
+        // function that will be invoked on chaincode instantiation
+    }
+
+    async requestDO(ctx, deliveryOrderData) {
+        const orderData = JSON.parse(deliveryOrderData);
+        const orderId = crypto.createHash('sha256').update(deliveryOrderData).digest('hex');
+        orderData.statusDate = String(new Date());
+        orderData.status = "Submitted";
+        orderData.orderId = orderId;
+        await ctx.stub.putState(orderId, Buffer.from(JSON.stringify(orderData)));
+        return { success: "OK", orderId: orderId, status: orderData.status, datetime: orderData.statusDate };
+    }
+
+    async getStatusDO(ctx, orderId) {
+        const buffer = await ctx.stub.getState(orderId);
+        if (!buffer || !buffer.length) {
+            return { error: "NOT_FOUND" }
+        }
+        const orderData = JSON.parse(buffer.toString())
+        return { success: "OK", status: orderData.status, datetime: orderData.statusDate }
+    }
+
+    async updateStatusDO(ctx, orderId, status, note) {
+        const buffer = await ctx.stub.getState(orderId);
+        if (!buffer || !buffer.length) {
+            return { error: "NOT_FOUND" }
+        }
+        const orderData = JSON.parse(buffer.toString())
+        // console.log(orderData) // jadi format json
+        orderData.status = status;
+        orderData.note = note;
+        orderData.statusDate = String(new Date());
+        await ctx.stub.putState(orderId, Buffer.from(JSON.stringify(orderData)));
+        return { success: "OK", status: orderData.status, datetime: orderData.statusDate }
+    }
+
+    async updateDO(ctx, orderId, updatedDeliveryOrderData) {
+        const buffer = await ctx.stub.getState(orderId);
+        if (!buffer || !buffer.length) return { error: "NOT_FOUND" };
+        const orderData = JSON.parse(updatedDeliveryOrderData);
+        await ctx.stub.putState(orderId, Buffer.from(JSON.stringify(orderData)));
+        return { success: "OK", status: orderData.status, datetime: orderData.statusDate };
+    }
+
+    async releaseDO(ctx, orderId) {
+        const buffer = await ctx.stub.getState(orderId);
+        if (!buffer || !buffer.length) return { error: "NOT_FOUND" };
+        const orderData = JSON.parse(buffer.toString());
+        orderData.status = "Released";
+        orderData.statusDate = String(new Date())
+        await ctx.stub.putState(orderId, Buffer.from(JSON.stringify(orderData)));
+        return { success: "OK", status: orderData.status, datetime: orderData.statusDate };
+    }
+
+    async rejectDO(ctx, orderId) {
+        const buffer = await ctx.stub.getState(orderId);
+        if (!buffer || !buffer.length) return { error: "NOT_FOUND" };
+        const orderData = JSON.parse(buffer.toString());
+        orderData.status = "Rejected";
+        orderData.statusDate = String(new Date());
+        await ctx.stub.putState(orderId, Buffer.from(JSON.stringify(orderData)));
+        return { success: "OK", status: orderData.status, datetime: orderData.statusDate };
+    }
+
+    async queryOrderById(ctx, orderId) {
+        const buffer = await ctx.stub.getState(orderId);
+        if (!buffer || !buffer.length) return { error: "NOT_FOUND" };
+        return JSON.parse(buffer.toString());
+    }
+
+    async queryAllOrders(ctx) {
+        const startKey = '';
+        const endKey = '';
+        const allResults = [];
+        for await (const { key, value } of ctx.stub.getStateByRange(startKey, endKey)) {
+            const strValue = Buffer.from(value).toString('utf8');
+            let record = JSON.parse(strValue);
+            allResults.push({ Key: key, Record: record });
+        }
+        return allResults;
+    }
+
+    async queryAllOrdersCO(ctx, coName) {
+        const startKey = '';
+        const endKey = '';
+        let filterResults = []
+        for await (const { key, value } of ctx.stub.getStateByRange(startKey, endKey)) {
+            const strValue = Buffer.from(value).toString('utf8');
+            let record = JSON.parse(strValue);
+            if (record.requestDetail.requestor.requestorId === coName) {
+                filterResults.push({ Key: key, Record: record })
+            }
+        }
+        return filterResults
+    }
+
+    async queryAllOrdersSL(ctx, listKodeSL) {
+        const startKey = '';
+        const endKey = '';
+        let filterResults = [];
+        const listSLCode = JSON.parse(listKodeSL)
+        for await (const { key, value } of ctx.stub.getStateByRange(startKey, endKey)) {
+            const strValue = Buffer.from(value).toString('utf8');
+            let record = JSON.parse(strValue);
+            if (listSLCode.includes(record.requestDetail.shippingLine.shippingType.split("|")[0].trim())) {
+                filterResults.push({ Key: key, Record: record })
+            }
+        }
+        return filterResults
+    }
+
+    async queryAllOrdersRelease(ctx) {
+        const startKey = '';
+        const endKey = '';
+        let filterResults = [];
+        const listSLCode = JSON.parse(listKodeSL)
+        for await (const { key, value } of ctx.stub.getStateByRange(startKey, endKey)) {
+            const strValue = Buffer.from(value).toString('utf8');
+            let record = JSON.parse(strValue);
+            if (record.requestDetail.status === 'Released') {
+                filterResults.push({ Key: key, Record: record })
+            }
+        }
+        return filterResults
+    }
+
+    async deleteDO(ctx, orderId) {
+        const buffer = await ctx.stub.getState(orderId);
+        if (!buffer || !buffer.length) {
+            return { error: "NOT FOUND" }
+        }
+        const deletedBuffer = await ctx.stub.deleteState(orderId);
+        return { success: "OK" }
+    }
+}
+
+exports.contracts = [DOContract];
--- channelup.sh
+++ channelup.sh
@@ -9,6 +9,8 @@ docker exec cli peer channel join -b ./channel-artifacts/dochannel.block
 # menambahkan peer0.org2.sl.com ke dalam channel
 docker exec -e CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.sl.com/users/Admin@org2.sl.com/msp -e CORE_PEER_ADDRESS=peer0.org2.sl.com:9051 -e CORE_PEER_LOCALMSPID="Org2MSP" -e CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.sl.com/peers/peer0.org2.sl.com/tls/ca.crt cli peer channel join -b ./channel-artifacts/dochannel.block
 
+# update peer0.org1.co.com 
 docker exec cli peer channel update -o orderer.example.com:7050 -c dochannel -f ./channel-artifacts/Org1MSPanchors.tx --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
 
+# update peer0.org2.sl.com
 docker exec -e CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.sl.com/users/Admin@org2.sl.com/msp -e CORE_PEER_ADDRESS=peer0.org2.sl.com:9051 -e CORE_PEER_LOCALMSPID="Org2MSP" -e CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.sl.com/peers/peer0.org2.sl.com/tls/ca.crt cli peer channel update -o orderer.example.com:7050 -c dochannel -f ./channel-artifacts/Org2MSPanchors.tx --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
